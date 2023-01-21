b972dad57e0e677368bc3266a89238b79e68658f
Adam Rauch <adam@labkey.com>
2023-01-21 15:14:50 -0800

    Migrate setJsonObject() use

--- test/src/com/hphc/remoteapi/fdahpuserregws/AppPropertiesUpdateCommand.java
+++ test/src/com/hphc/remoteapi/fdahpuserregws/AppPropertiesUpdateCommand.java
@@ -1,14 +1,23 @@
 package com.hphc.remoteapi.fdahpuserregws;
 
 import com.hphc.remoteapi.fdahpuserregws.params.AppPropertiesDetails;
+import org.json.JSONObject;
 import org.labkey.remoteapi.CommandResponse;
 import org.labkey.remoteapi.PostCommand;
 
 public class AppPropertiesUpdateCommand extends PostCommand<CommandResponse>
 {
+    private final JSONObject _json;
+
     public AppPropertiesUpdateCommand(AppPropertiesDetails appProperties)
     {
         super(BaseRegistrationCommand.CONTROLLER, "appPropertiesUpdate");
-        setJsonObject(appProperties.toJSONObject());
+        _json = appProperties.toJSONObject();
+    }
+
+    @Override
+    public JSONObject getJsonObject()
+    {
+        return _json;
     }
 }
